2f74ed95b321f0b27d69d40714728e14410c9c72
Nyaundi <jamesmabuka260@gmail.com>
2024-03-15 19:10:10 +0300

    Add project files.

---
+++ TestApp/Program.cs
@@ -0,0 +1,39 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        //Arithmetic Operators
+        decimal a = 10M;
+        decimal b = 3M;
+        decimal c = a + b;
+        decimal d = a - b;
+        decimal e = a * b;
+        decimal f = a / b;
+        decimal g = a % b;
+        System.Console.WriteLine(c);    
+        System.Console.WriteLine(d);    
+        System.Console.WriteLine(e);
+        System.Console.WriteLine(f);
+        System.Console.WriteLine(g);
+
+        //Assignemnt operatoros
+        a += 20M;
+        System.Console.WriteLine(a);
+
+        a -= 20M;
+        System.Console.WriteLine(a);
+
+        a *= 3M;
+        System.Console.WriteLine(a);
+
+        a /= 3M;
+        System.Console.WriteLine(a);
+        //Increment /Decrement Operators
+        a = 10M;
+        System.Console.WriteLine(++a);
+        System.Console.WriteLine(a++);
+        System.Console.WriteLine(a);    
+
+        System.Console.ReadKey();
+    }
+}
