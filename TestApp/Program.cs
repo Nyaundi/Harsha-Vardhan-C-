3cadfb9cc3b317680c0c13e1b479fbc6a3703ce5
Nyaundi <jamesmabuka260@gmail.com>
2024-03-18 12:04:56 +0300

    Assignment 1

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,53 +1,19 @@
-﻿class Sample
+﻿
+class Assignmentareaofacircle
 {
     static void Main()
     {
-        //Arithmetic Operators
-        decimal a = 10M;
-        decimal b = 3M;
-        decimal c = a + b;
-        decimal d = a - b;
-        decimal e = a * b;
-        decimal f = a / b;
-        decimal g = a % b;
-        System.Console.WriteLine(c);    
-        System.Console.WriteLine(d);    
-        System.Console.WriteLine(e);
-        System.Console.WriteLine(f);
-        System.Console.WriteLine(g);
+        //inputs 
+        double pi = 3.1415; //constant pi
+        int radius = 5;
 
-        //Assignemnt operatoros
-        a += 20M;
-        System.Console.WriteLine(a);
+        //formula
 
-        a -= 20M;
-        System.Console.WriteLine(a);
+        double area = pi * radius *radius;
 
-        a *= 3M;
-        System.Console.WriteLine(a);
+        //output
 
-        a /= 3M;
-        System.Console.WriteLine(a);
-        //Increment /Decrement Operators
-        a = 10M;
-        System.Console.WriteLine(++a);
-        System.Console.WriteLine(a++);
-        System.Console.WriteLine(a);
-
-        //Concatenation Operator
-        string name = "Scott";
-        int age = 10;
-        string message = "Hey, " + name + ", your age is " + age;
-        System.Console.WriteLine(message);
-
-
-        //ternary operator
-        string title = (age < 13) ? "Child" : (age >= 13 && age <= 19) ? "Teenager" : "Adult";
-        System.Console.WriteLine(title);
-
-        //operator precedence
-        double z = 10 + 4 * 30 / 10;
-        System.Console.WriteLine(z);
-        System.Console.ReadKey();
+        System.Console.WriteLine(area);
+        System.Console.Read();
     }
 }
