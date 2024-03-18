bf17fd1ea8c2c9a6b711b83b7477a43b9c05c47f
Nyaundi <jamesmabuka260@gmail.com>
2024-03-18 12:58:07 +0300

    Assignemnt 2

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,19 +1,50 @@
-﻿
-class Assignmentareaofacircle
+﻿//class Assignement
+//{
+//    static void Main()
+//    {
+//        double pi = 22 / 7;
+//        int radius = 7;
+
+//        //Calculation
+//        double area = pi * radius * radius;
+//        System.Console.WriteLine(area);
+
+//        System.Console.ReadLine();
+//    }
+//}
+
+//Write a C# program convert a person's height (in feet and inches) to centimeters.
+
+/*
+Write a C# program convert a person's height (in feet and inches) to centimeters.
+Eg:
+feet = 5
+inches = 7
+output: 170.18 cm
+
+Formula:
+1 inch = 2.54 cm
+1 feet = 12 inches
+*/
+using System.ComponentModel;
+
+class Program
 {
     static void Main()
     {
-        //inputs 
-        double pi = 3.1415; //constant pi
-        int radius = 5;
+        int feet = 5;
+        int inches = 7;
 
-        //formula
 
-        double area = pi * radius *radius;
+        //find tital number of inches based on feet
 
-        //output
+        int total_inches = (feet * 12) + inches;
+        double cm = total_inches * 2.54;
 
-        System.Console.WriteLine(area);
+        //Output
+
+        System.Console.WriteLine(cm);
         System.Console.Read();
     }
+    
 }
